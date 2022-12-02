7d80a6afd5697edce4ecf94451733a0f527b7bf4
seanmartz <sean@seanmartz.com>
2022-12-02 10:56:30 -0500

    solve day 1 part 1

---
+++ day01/Program.fs
@@ -0,0 +1,19 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System.IO
+
+let fullPath =
+    Path.Combine(__SOURCE_DIRECTORY__, "input.txt")
+
+
+
+
+let data = File.ReadAllText(fullPath).Split("\n\n")
+            |> List.ofArray
+            |> List.map (fun s -> s.Split "\n" |> List.ofArray)
+            |> List.map (fun sl -> sl  |> List.map int |> List.sum)
+            |> List.sortDescending
+            |> List.head
+
+
+
+printf "%d" data
