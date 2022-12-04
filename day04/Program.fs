7ecb5d4a22b9b516488e7eb540db908b7e4d634e
seanmartz <sean@seanmartz.com>
2022-12-04 15:50:40 -0500

    day 4 part 2

--- day04/Program.fs
+++ day04/Program.fs
@@ -13,6 +13,9 @@ let q =
     |> List.map (fun ((a, b), (c, d)) -> ((int a, int b), (int c, int d))) // ((2,4),(6,8))
     |> List.map (fun ((b1, e1), (b2, e2)) -> (seq { b1..e1 }, seq { b2..e2 })) // ([2,3,4],[6,7,8])
     |> List.map (fun (s1, s2) -> (s1 |> Set.ofSeq, s2 |> Set.ofSeq)) // ({2,3,4},{6,7,8})
+
+let answer1 =
+    q
     |> List.map (fun (a, b) ->
         if (a.Count < b.Count) then
             (b, a)
@@ -21,4 +24,11 @@ let q =
     |> List.map (fun (s1, s2) -> Set.isSubset s2 s1)
     |> List.filter (fun r -> r)
 
-printfn $"Answer {q.Length}"
+printfn $"Answer {answer1.Length}"
+
+let answer2 =
+    q
+    |> List.map (fun (fst, snd) -> Set.intersect fst snd)
+    |> List.filter (fun set -> not set.IsEmpty)
+
+printfn $"Answer2 {answer2.Length}"
